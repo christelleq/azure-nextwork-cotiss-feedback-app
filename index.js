486cdbe130fdc50dce5f59dd97d95722c98de9c9
Christelle Quilang <christellequilang@protonmail.com>
2022-12-12 20:29:33 +1300

    Update db

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
+// Use express
+import express, { json, urlencoded } from 'express'
+const app = express()
+app.use(json())
+app.use(urlencoded({ extended: true }))
+import path from 'path'
+const __dirname = path.resolve()
+
 // Read .env file and set environment variables
-require('dotenv').config()
+// require('dotenv').config()
 const random = Math.floor(Math.random() * 100)
 
 // Use official mongodb driver to connect to the server
-const { MongoClient, ObjectId } = require('mongodb')
+import { MongoClient, ObjectId } from 'mongodb'
 
 // New instance of MongoClient with connection string
 // for Cosmos DB
-const url = process.env.COSMOS_CONNECTION_STRING
+const url = process.env.REACT_APP_COSMOS_CONNECTION_STRING
 const client = new MongoClient(url)
 
 // Database reference with creation if it does not already exist
@@ -17,6 +25,22 @@ console.log(`Database:\t${db.databaseName}\n`)
 // Collection reference with creation if it does not already exist
 const collection = db.collection('feedback')
 console.log(`Collection:\t${collection.collectionName}\n`)
+
+app.get('/', function (req, res) {
+  res.sendFile('index.html', { root: path.join(__dirname) })
+})
+
+app.post('/', async function (req, res) {
+  console.log('post', req.body.feedback)
+  const newFeedback = {
+    date: new Date(),
+    feedback: req.body.feedback,
+  }
+  console.log(new Date())
+  await client.connect()
+  const newfb = await collection.insertOne(newFeedback)
+  console.log(JSON.stringify(newfb))
+})
 
 async function main() {
   // The remaining operations are added here
@@ -29,12 +53,14 @@ async function main() {
   // - without sharding, should use {_id}
   // - with sharding,    should use {_id, partitionKey }, ex: {_id, category}
   const feedbackPiece = await collection.findOne({
-    _id: ObjectId('63967a2744f5570ce0e23e94'),
+    _id: ObjectId('6396d6a0f4945cb5ac7438e8'),
   })
   console.log(`feedbackPiece: ${JSON.stringify(feedbackPiece)}\n`)
 }
 
 main()
-  .then(console.log)
+  .then(console.log('main'))
   .catch(console.error)
   .finally(() => client.close())
+
+app.listen(3000)
